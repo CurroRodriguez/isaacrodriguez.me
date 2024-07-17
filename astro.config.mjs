ff8be1b7760f3dabf0835583f15b1f6937273792
Isaac Rodriguez <isaac_rodriguez@live.com>
2024-07-17 19:47:53 -0400

    Configured Adapter for SSR on Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://isaacrodriguez.me"
+  site: "https://isaacrodriguez.me",
+  output: "server",
+  adapter: netlify()
 });
